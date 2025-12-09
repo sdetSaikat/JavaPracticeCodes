20a3d4819f39fefb24f1790bcc604bcc5d16b35f
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-09 19:41:05 +0530

    adding the Hashmap code

---
+++ src/treeConcepts/HashMapConcepts.java
@@ -0,0 +1,14 @@
+package treeConcepts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapConcepts {
+
+	public static void main(String[] args) {
+		Map<String,Integer> hm =new HashMap<String,Integer>();
+		hm.put("Saikat", 34);
+		hm.put("subh", 35);
+	}
+
+}
