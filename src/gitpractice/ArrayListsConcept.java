a3c0ba358715335a9c4e08e98822f8d543060061
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-09 20:52:23 +0530

    Hello

---
+++ src/gitpractice/ArrayListsConcept.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class ArrayListsConcept {
+	
+	
+	public void qaFundamentals() {
+		System.out.println("Qa has be more proactive");
+	}
+
+}
--- src/gitpractice/ArraylistConcepts.java
+++
@@ -1,9 +0,0 @@
-package gitpractice;
-
-public class ArraylistConcepts {
-	
-	public void testing() {
-		System.out.println("testing has to be in a correct way");
-	}
-
-}
