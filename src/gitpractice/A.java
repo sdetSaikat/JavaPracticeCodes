85af8d6f2e0f25172c317ecbc5a567d96ba2f44d
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-10 13:52:44 +0530

    check3

--- src/gitpractice/A.java
+++ src/gitpractice/A.java
@@ -3,6 +3,7 @@ package gitpractice;
 public class A {
 
 
+
 	public static void main(String[] args) {
 	System.out.println("in class A");
   
@@ -14,6 +15,11 @@ public class A {
   }
 
 
+
+	public void inoopsConceptBranch() {
+		System.out.println("in oopsConcept method ");
+	}
+	
 
 }
 
