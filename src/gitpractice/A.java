bdc3636298b9800cc360d08ad0fca29886620f6e
Saikat Maity <saikatmaity@Saikats-MacBook-Pro.local>
2025-12-10 13:39:28 +0530

    check2

--- src/gitpractice/A.java
+++ src/gitpractice/A.java
@@ -2,13 +2,18 @@ package gitpractice;
 
 public class A {
 
+
 	public static void main(String[] args) {
 	System.out.println("in class A");
   
 
 	}
 	
+  public void inLocal(){
+   System.out.println("I am practicing in my local");
+  }
 
 
-	}
+
+}
 
