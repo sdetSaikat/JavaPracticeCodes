35e892873463144bf579914bb22a4b4911c609ac
sdetSaikat <talktosaikat9@gmail.com>
2025-12-10 13:30:15 +0530

    Create A.java

---
+++ src/gitpractice/A.java
@@ -0,0 +1,15 @@
+package gitpractice;
+
+public class A {
+
+	public static void main(String[] args) {
+	System.out.println("in class A");
+  
+
+	}
+
+  public void practice(){
+   System.out.println("in practice method");
+  }
+
+}
